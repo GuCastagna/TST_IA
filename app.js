f82ea54092a977083136e66a554ee7476a148241
Gustavo Batista Castagna <gustavobatista.castagna@gmail.com>
2024-09-07 10:09:22 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 function pesquisar() {
-    // Seleciona a seção onde os resultados serão exibidos
-    let section = document.getElementById("resultados-pesquisa");
-    // Imprime a seção no console para verificar se foi selecionada corretamente
-    console.log(section);
+  // Seleciona a seção onde os resultados serão exibidos
+  let section = document.getElementById("resultados-pesquisa");
+  let campoPesquisa = document.getElementById("campo-pesquisa").value;
 
-    // Inicializa uma string vazia para armazenar os resultados
-    let resultados = "";
+  // Imprime a seção no console para verificar se foi selecionada corretamente
+  console.log(campoPesquisa);
 
-    // Itera sobre cada dado da lista de dados
-    for (let dado of dados) {
+  // Inicializa uma string vazia para armazenar os resultados
+  let resultados = "";
+  let titulo = "";
+  let descricao = "";
+  //let tags = "";
+
+  //Se o campoPesquisa for um vazio.
+  if(campoPesquisa ==""){
+    section.innerHTML = "<p>Por favor, escreva algo na pesquisa que esteja relacionado a Segurança do Trabalho.</p>";
+    return;
+  }
+  campoPesquisa = campoPesquisa.toLowerCase();
+
+  // Itera sobre cada dado da lista de dados
+  for (let dado of dados) {
+    titulo = dado.titulo.toLowerCase();
+    descricao = dado.descricao.toLowerCase();
+    //tags = dado.tags.toLowerCase();
+    if(titulo.includes(campoPesquisa)||descricao.includes(campoPesquisa)/*||tags.includes(campoPesquisa)*/){
         // Cria um novo elemento div para cada dado e adiciona as informações
         resultados += `
         <div class="item-resultado">
@@ -24,10 +40,9 @@ function pesquisar() {
         </div>
       `;
     }
+  }
 
-    // Atribui a string com os resultados ao conteúdo da seção
-    section.innerHTML = resultados;
+  // Atribui a string com os resultados ao conteúdo da seção
+  section.innerHTML = resultados;
 }
 //console.log(dados);
-
-
--- dados.js
+++ dados.js
@@ -1,82 +1,91 @@
 let dados = [{
     titulo: "EPI",
     descricao: "O Equipamento de Proteção Individual (EPI) é fundamental para garantir a segurança e a saúde dos trabalhadores em diversos ambientes de trabalho. Ele é um dispositivo de uso individual, projetado para proteger o trabalhador contra riscos que possam ameaçar sua integridade física. A escolha do EPI adequado depende do tipo de atividade e dos riscos específicos a que o trabalhador está exposto. O uso correto e regular do EPI é obrigatório por lei e contribui significativamente para a prevenção de acidentes e doenças ocupacionais.",
-    link: "https://sindag.org.br/colunas_sindag/epi-significado-tipos-exemplo-e-sua-importancia/#:~:text=EPI%20%C3%A9%20o%20significado%20de,seguran%C3%A7a%20e%20a%20sua%20sa%C3%BAde"
+    link: "https://sindag.org.br/colunas_sindag/epi-significado-tipos-exemplo-e-sua-importancia/#:~:text=EPI%20%C3%A9%20o%20significado%20de,seguran%C3%A7a%20e%20a%20sua%20sa%C3%BAde", 
+    tags: "equipamento proteção individual segurança saúde"
 }, {
     titulo: "PPRA",
     descricao: "O PPRA (Plano de Prevenção de Riscos de Acidentes) é um conjunto de medidas que tem como objetivo preservar a saúde e a integridade física dos trabalhadores. O PPRA é regulamentado pela norma regulamentadora (NR) 9, emitida pelo Ministério do Trabalho e Emprego.",
-    link: "https://www.fiocruz.br/biosseguranca/Bis/virtual%20tour/hipertextos/up1/ppra.html#:~:text=PPRA%20%2D%20Programa%20de%20Preven%C3%A7%C3%A3o%20de%20Riscos%20Ambientais&text=Os%20riscos%20ambientais%20s%C3%A3o%20aqueles,danos%20%C3%A0%20sa%C3%BAde%20do%20trabalhador"
+    link: "https://www.fiocruz.br/biosseguranca/Bis/virtual%20tour/hipertextos/up1/ppra.html#:~:text=PPRA%20%2D%20Programa%20de%20Preven%C3%A7%C3%A3o%20de%20Riscos%20Ambientais&text=Os%20riscos%20ambientais%20s%C3%A3o%20aqueles,danos%20%C3%A0%20sa%C3%BAde%20do%20trabalhador",
+    tags: " plano prevenção riscos acidentes ambiente trabalho obras canteiro casa escritório"
 }, {
     titulo: "PPCI",
     descricao: "O PPCI é uma sigla que significa plano de prevenção e proteção de combate a incêndio, baseado em um conjunto de ações que possam garantir a segurança das pessoas. Esse plano é exigido por lei, sendo um documento fiscalizado pelo Corpo de Bombeiros para verificar se as instalações e implementações realizadas no local estão de acordo com as normas vigentes.",
-    link: "https://www.produttivo.com.br/blog/ppci-plano-de-prevencao-e-protecao-contra-incendios/"
-}, {
-    titulo: "Acidentes de Trabalho",
-    descricao: "Trata-se de um evento que ocorre no local de trabalho ou em decorrência do trabalho, resultando em lesões ou perturbações funcionais que podem causar a morte, a perda ou redução da capacidade de trabalho ou de ganho.",
-    link: "https://tst.jus.br/web/trabalhoseguro/resolucao#:~:text=Conforme%20disp%C3%B5e%20o%20art.%2019,ou%20a%20perda%20ou%20redu%C3%A7%C3%A3o%2C"
+    link: "https://www.produttivo.com.br/blog/ppci-plano-de-prevencao-e-protecao-contra-incendios/",
+    tags: "plano prevenção contra incêndios residência obras cidade"
 }, {
     titulo: "Ergonomia",
     descricao: "Estudo das relações entre o homem e o seu trabalho, buscando adaptar as condições de trabalho às características físicas e psicológicas do trabalhador, visando prevenir doenças ocupacionais.",
-    link: "https://www.ergonomia.org.br/"
-},
-{
+    link: "https://rockcontent.com/br/blog/ergonomia-no-trabalho/#:~:text=A%20ergonomia%20no%20trabalho%20%C3%A9%20uma%20%C3%A1rea%20de%20estudo%20que,LERs%2C%20dores%20ou%20qualquer%20dificuldade./",
+    tags: "ergonomia postura trabalho saúde física prevenção"
+},{
     titulo: "Riscos Químicos",
-    descricao: "São aqueles originados de substâncias químicas que podem causar danos à saúde dos trabalhadores, como irritação, alergias, intoxicações e doenças crônicas. Os agentes agressores podem ocorrer tanto em pele, olhos, boca e nariz, podendo causar a morte.",
-    link: "https://www.gov.br/trabalho-e-emprego/pt-br/assuntos/inspecao-do-trabalho/seguranca-e-saude-no-trabalho/riscos-quimicos"
-},
-{
-    titulo: "Riscos Físicos",
-    descricao: "São aqueles originados de agentes físicos como ruído, vibração, radiações, temperaturas extremas e umidade. Entra no quadro da insalubridade como também da periculosidade.",
-    link: "https://www.gov.br/trabalho-e-emprego/pt-br/assuntos/inspecao-do-trabalho/seguranca-e-saude-no-trabalho/riscos-fisicos"
-},
-{
+    descricao: "São aqueles originados de substâncias químicas que podem causar danos à saúde dos trabalhadores, como irritação, alergias, intoxicações e doenças crônicas.",
+    link: "https://onsafety.com.br/riscos-quimicos/#:~:text=O%20risco%20qu%C3%ADmico%20%C3%A9%20um,s%C3%A3o%20danosos%20%C3%A0%20nossa%20sa%C3%BAde.",
+    tags: "riscos químicos substâncias toxicas segurança industrial"
+},{
+    titulo: "Saúde Mental no Trabalho",
+    descricao: "Aborda a importância de cuidar da saúde mental dos trabalhadores, prevenindo doenças como burnout, ansiedade e depressão, que podem afetar o desempenho e a qualidade de vida.",
+    link: "https://www.saude.gov.br/saude-do-trabalhador/saude-mental-no-trabalho",
+    tags: "saude mental trabalho bem-estar psicologico"
+},{
+    titulo: "Acidentes de Trabalho com Eletricidade",
+    descricao: "Aborda os riscos da eletricidade no ambiente de trabalho, as medidas de prevenção e os procedimentos em caso de acidentes.",
+    link: "https://www.segurancadotrabalho.net/acidentes-com-eletricidade/",
+    tags: "eletricidade choque eletrico segurança elétrica"
+},{
+    titulo: "Trabalho em Altura",
+    descricao: "Aborda os riscos e cuidados necessários para realizar trabalhos em altura, como a utilização de equipamentos de proteção individual e coletiva.",
+    link: "https://www.segurancadotrabalho.net/acidentes-de-trabalho-em-altura/",
+    tags: "trabalho em altura queda segurança"
+},{
+    titulo: "Saúde Ocupacional",
+    descricao: "Campo da saúde pública que se dedica à promoção e proteção da saúde dos trabalhadores, incluindo a prevenção de doenças relacionadas ao trabalho.",
+    link: "https://www.saude.gov.br/saude-do-trabalhador",
+    tags: "saude ocupacional prevenção doenças trabalho"
+},{
+    titulo: "Assédio Moral no Trabalho",
+    descricao: "Qualquer conduta abusiva (atos, gestos, palavras, comportamentos) que humilhe, ofenda, constranja, ameace ou vilipende a dignidade ou integridade física ou psicológica de um trabalhador.",
+    link: "https://www.trt15.jus.br/imprensa/noticias/noticias-do-trt/ass%C3%A9dio-moral-no-trabalho-o-que-e-e-como-combater",
+    tags: "assédio moral trabalho direitos humanos psicologia"
+},{
     titulo: "Riscos Ergonômicos",
-    descricao: "São aqueles relacionados à organização do trabalho, às condições ambientais e aos aspectos psicossociais que podem levar a doenças osteomusculares, distúrbios do sistema nervoso e transtornos mentais.",
-    link: "https://www.gov.br/trabalho-e-emprego/pt-br/assuntos/inspecao-do-trabalho/seguranca-e-saude-no-trabalho/riscos-ergonomicos"
-},
-{
-    titulo: "Riscos Biológicos",
-    descricao: "São aqueles provenientes de agentes biológicos como bactérias, vírus, fungos e parasitas, que podem causar doenças infecciosas. Os agentes se enquadram tanto na parte de insalubridade como também na periculosidade.",
-    link: "https://www.gov.br/trabalho-e-emprego/pt-br/assuntos/inspecao-do-trabalho/seguranca-e-saude-no-trabalho/riscos-biologicos"
-},
-{
-    titulo: "Acidentes de Trabalho com Máquinas e Equipamentos",
-    descricao: "Acidentes causados pelo contato com máquinas e equipamentos, como prensas, máquinas operatrizes, elevadores e outros. O tipo de agente que mais causa afastamentos de trabalho e falência de trabalhadores.",
-    link: "https://www.segurancadotrabalho.net/acidentes-com-maquinas/"
-},
-{
-    titulo: "Acidentes de Trabalho em Altura",
-    descricao: "Acidentes ocorridos em locais elevados, como quedas de altura, impactos contra objetos fixos e outros. Não só causam danos físicos como podem causar falência do trabalhador. ",
-    link: "https://www.segurancadotrabalho.net/acidentes-de-trabalho-em-altura/"
-},
-{
-    titulo: "Proteção Respiratória",
-    descricao: "Equipamentos de proteção individual utilizados para proteger as vias respiratórias contra a inalação de agentes contaminantes. A proteção é essencial, pois pode gerar danos nasais como também risco de vida.",
-    link: "https://www.3m.com.br/3m/pt-br/seguranca-industrial/protecao-respiratoria/"
-},
-{
-    titulo: "Proteção Auditiva",
-    descricao: "Equipamentos de proteção individual utilizados para proteger a audição dos trabalhadores contra ruídos excessivos. A saúde auditiva deve ser verificada a cada 6 meses, para monitorar se houver algum dano auditivo a longo ou curto prazo.",
-    link: "https://www.3m.com.br/3m/pt-br/seguranca-industrial/protecao-auditiva/"
-},
-{
-    titulo: "Proteção Ocular",
-    descricao: "Equipamentos de proteção individual utilizados para proteger os olhos contra impactos, radiações, produtos químicos e outros agentes nocivos ao globo ocular. Vale lembrar que os agentes podem causar danos oculares e causar a morte.",
-    link: "https://www.3m.com.br/3m/pt-br/seguranca-industrial/protecao-ocular/"
-},
-{
-    titulo: "Programa de Controle Médico de Saúde Ocupacional (PCMSO)",
-    descricao: "Conjunto de medidas que objetivam a promoção e a preservação da saúde dos trabalhadores, através da realização de exames médicos periódicos e acompanhamento da saúde dos trabalhadores. O programa abrange desde análises físicas como também análises psiquicas.",
-    link: "https://www.gov.br/trabalho-e-emprego/pt-br/assuntos/inspecao-do-trabalho/seguranca-e-saude-no-trabalho/pcmso"
-},
-{
-    titulo: "Programa de Prevenção de Riscos Ambientais (PPRA)",
-    descricao: "Conjunto de medidas que tem como objetivo a preservação da saúde e a integridade física dos trabalhadores, através da avaliação e controle dos riscos ambientais. O ambiente de trabalho pode ser tanto insalubre como risco de vida, a planificação é essencial para permitir um ambiente seguro tanto ao trabalhador como também o empregador.",
-    link: "https://www.gov.br/trabalho-e-emprego/pt-br/assuntos/inspecao-do-trabalho/seguranca-e-saude-no-trabalho/ppra"
-},
-{
-    titulo: "CIPA (Comissão Interna de Prevenção de Acidentes)",
-    descricao: "Órgão paritário, permanente e de caráter consultivo e deliberativo, composto por representantes do empregador e dos empregados, com a finalidade de prevenir acidentes e doenças ocupacionais. ",
-    link: "https://www.gov.br/trabalho-e-emprego/pt-br/assuntos/inspecao-do-trabalho/seguranca-e-saude-no-trabalho/cipa"
-}
-]
+    descricao: "Fatores relacionados ao trabalho que podem causar doenças osteomusculares, distúrbios do sistema nervoso e transtornos mentais.",
+    link: "https://www.gov.br/trabalho-e-emprego/pt-br/assuntos/inspecao-do-trabalho/seguranca-e-saude-no-trabalho/riscos-ergonomicos",
+    tags: "ergonomia postura trabalho repetitivo"
+},{
+    titulo: "Acidentes com Veículos",
+    descricao: "Acidentes envolvendo veículos motorizados no ambiente de trabalho, como caminhões, empilhadeiras e outros.",
+    link: "https://www.segurancadotrabalho.net/acidentes-com-veiculos/",
+    tags: "veículos transporte segurança direção defensiva"
+},{
+    titulo: "Proteção Radiológica",
+    descricao: "Conjunto de medidas para proteger os trabalhadores expostos a radiações ionizantes e não ionizantes.",
+    link: "https://www.cnen.gov.br/areas-de-atuacao/protecao-radiologica",
+    tags: "radiação proteção radiológica saúde nuclear"
+},{
+    titulo: "Primeiros Socorros",
+    descricao: "Conjunto de procedimentos de emergência que devem ser realizados em caso de acidente ou mal súbito, antes da chegada de socorro médico.",
+    link: "https://www.sbp.org.br/primeiros-socorros/",
+    link: "primeiros socorros emergência saúde"
+},{
+    titulo: "Espaços Confinados",
+    descricao: "Locais de trabalho com acesso limitado e ventilação natural insuficiente, que apresentam riscos de asfixia, intoxicação e outros.",
+    link: "https://www.segurancadotrabalho.net/espacos-confinados/",
+    tags: "espaços confinados risco de vida segurança"
+},{
+    titulo: "Gestão de Riscos",
+    descricao: "Processo contínuo de identificação, análise e avaliação de riscos, com o objetivo de implementar medidas de controle e prevenção.",
+    link: "https://www.abnt.org.br/noticias/gest%C3%A3o-de-riscos-uma-ferramenta-essencial-para-a-seguranca-do-trabalho",
+    tags: "gestão de riscos segurança prevenção"
+},{
+    titulo: "Cultura de Segurança",
+    descricao: "Um conjunto de valores, crenças e comportamentos que priorizam a segurança em todos os níveis da organização.",
+    link: "https://www.segurancadotrabalho.net/cultura-de-seguranca/",
+    tags: "cultura de segurança prevenção acidentes"
+},{
+    titulo: "Acidentes com Ferramentas",
+    descricao: "Acidentes causados pelo uso inadequado de ferramentas manuais e elétricas.",
+    link: "https://www.segurancadotrabalho.net/acidentes-com-ferramentas/",
+    tags: "ferramentas acidentes segurança trabalho"
+}]
